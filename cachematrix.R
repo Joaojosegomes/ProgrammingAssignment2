bb4a6fb95b33d25d42f6ff54ff8051cebed96296
Joaojosegomes <51260119+Joaojosegomes@users.noreply.github.com>
2019-06-04 16:25:22 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Supposing than matrix has ever inverse
+## we have a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
 
+##This function "makeCacheMatrix" gets a matrix as an input, set the value of the matrix,
+#get the value of the matrix, set the inverse Matrix and get the inverse Matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+      invMatrix <- NULL
+  
+      #set the value of the Matrix
+      setMatrix <- function(y) {
+            x <<- y
+            invMatrix <<- NULL
+      }
+  
+      getMatrix <- function() x                              #get the value of the Matrix
+  
+      setInverse <- function(inverse) invMatrix <<- inverse  #set the value of the inverse
+  
+      getInverse <- function() invMatrix                     #get the value of the inverse
+  
+      list(setMatrix = setMatrix, getMatrix = getMatrix,
+            setInverse = setInverse, getInverse = getInverse)
+  
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      
+      m <- x$getinv()
+      
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      
+      data <- x$get()
+      
+      m <- solve(data, ...)
+      
+      x$setinv(m)
+      
+      return(round(m,digits=2))
 }
